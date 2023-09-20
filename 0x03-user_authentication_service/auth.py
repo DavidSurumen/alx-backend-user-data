9874dba3dc2d33e9056e1b59ab70e4e5cb7165bf
David Surumen <davidsurumen@gmail.com>
2023-09-20 17:51:44 +0300

    task 16 -implement password reset generator

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,3 +100,18 @@ class Auth:
             return
         except Exception:
             return
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Creates a password reset token
+        """
+        if type(email) is not str:
+            raise TypeError('email needs to be a string')
+
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            user.reset_token = reset_token
+            return reset_token
+        except NoResultFound:
+            raise ValueError
