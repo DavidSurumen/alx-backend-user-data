3955238e17d53c7fa7e3ba074a3d70b6be828016
David Surumen <davidsurumen@gmail.com>
2023-09-22 17:14:21 +0300

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Filtered logger module
+"""
+import logging
+import re
+from typing import List
+
+
+patterns = {
+        'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+        'replace': lambda x: r'\g<field>={}'.format(x),
+        }
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Filters a log file
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
