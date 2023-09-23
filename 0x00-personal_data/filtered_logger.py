97723b4567158d07e67055e01aa2520717949117
David Surumen <davidsurumen@gmail.com>
2023-09-23 14:35:55 +0300

    task 1 -implement log record formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,9 @@ from typing import List
 
 
 patterns = {
-        'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-        'replace': lambda x: r'\g<field>={}'.format(x),
+        'extract': lambda fields, separator: r'(?P<field>{})=[^{}]*'.
+        format('|'.join(fields), separator),
+        'replace': lambda redaction: r'\g<field>={}'.format(redaction),
         }
 
 
@@ -20,3 +21,23 @@ def filter_datum(fields: List[str], redaction: str,
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats a LogRecord.
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
