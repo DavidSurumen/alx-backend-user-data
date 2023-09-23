c2b41c13ce5d9ecab12065b7a2671bfd55ef9ccf
David Surumen <davidsurumen@gmail.com>
2023-09-23 17:21:33 +0300

    task 6 -validate password function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Hashes password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
