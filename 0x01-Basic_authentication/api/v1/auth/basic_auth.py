ea2b89947d833010e3c1e82dfa705c27fb9c5997
David Surumen <davidsurumen@gmail.com>
2023-09-25 00:35:45 +0300

    task 7 -extract Base64 portion of authorization key

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,14 @@ class BasicAuth(Auth):
     """
     Basic Authentication class
     """
+    def extract_base64_authorization_header(self,
+            authorization_header:str) -> str:
+        """
+        Extracts the Base64 part of the authorization header
+        """
+        if not authorization_header or type(authorization_header) != str:
+            return None
+        key = authorization_header.split(' ')
+        if key[0] != 'Basic':
+            return None
+        return key[1]
