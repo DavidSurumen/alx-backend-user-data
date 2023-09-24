d0c91c20e89a903cb0aaff3378e0aeb7a482088f
David Surumen <davidsurumen@gmail.com>
2023-09-25 00:55:37 +0300

    task 8 -decoding a base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
         if key[0] != 'Basic':
             return None
         return key[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes base64 string
+        """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except base64.binascii.Error:
+            return None
