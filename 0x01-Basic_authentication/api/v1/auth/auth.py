dc0d12be3a887ae7911e073626c17993c120dd97
David Surumen <davidsurumen@gmail.com>
2023-09-24 18:47:07 +0300

    task 3 -request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Checks whether authentication is required
+
+        Return:
+            True -> If path is not in excluded_paths, or path is None, or
+                    excluded_list is None, authenticate.
+
+            False -> If path is in excluded_paths, no authentication.
         """
         if not excluded_paths or len(excluded_paths) == 0:
             return True
@@ -34,7 +40,12 @@ class Auth:
         Parameters:
             request - flask request object
         """
-        return None
+        if not request:
+            return None
+        auth_key = request.headers.get('Authorization')
+        if not auth_key:
+            return None
+        return auth_key
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
