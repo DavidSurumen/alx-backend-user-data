a52d10472565ed7b37d4ddaf82799b2ab1eee9ae
David Surumen <davidsurumen@gmail.com>
2023-09-25 12:21:47 +0300

    task 9 -extract user email and password from header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,18 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts email and password from authorization header
+        """
+        if not decoded_base64_authorization_header:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        email = decoded_base64_authorization_header.split(':')[0]
+        pwd = decoded_base64_authorization_header.split(':')[1]
+        return (email, pwd)
