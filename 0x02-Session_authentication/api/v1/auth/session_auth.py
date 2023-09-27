56a32f9aa2f1df458ac437f212ccfcf1c8e5430a
David Surumen <davidsurumen@gmail.com>
2023-09-27 21:15:54 +0300

    map session id to user id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,20 @@
 Session authentication module
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session id for a user id
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+        sess_id = str(uuid.uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id
