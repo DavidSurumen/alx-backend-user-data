97da036c87e7212ac26042cff6d3ba774f44ff1e
David Surumen <davidsurumen@gmail.com>
2023-09-28 09:41:16 +0300

    complete task 3 -method returning userID for given sessionID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,11 @@ class SessionAuth(Auth):
         sess_id = str(uuid.uuid4())
         self.user_id_by_session_id[sess_id] = user_id
         return sess_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Gets a user associated with a session
+        """
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
