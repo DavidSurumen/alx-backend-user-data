f412ea060d115b368d46c97b47584508f3dc6587
David Surumen <davidsurumen@gmail.com>
2023-09-28 17:47:25 +0300

    added current_user method to retrieve User instance based on session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@ Session authentication module
 """
 from .auth import Auth
 import uuid
+from typing import TypeVar
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -28,3 +30,11 @@ class SessionAuth(Auth):
         if not session_id or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overrides Auth.current_user.
+        Retrieves a user instance associated with a session.
+        """
+        session_id = self.session_cookie(request)
+        return User.get(self.user_id_for_session_id(session_id))
