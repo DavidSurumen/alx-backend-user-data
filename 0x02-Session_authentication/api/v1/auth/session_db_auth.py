df1051e7fbb20f34ad90cd9ec63c06d4a65954c1
David Surumen <davidsurumen@gmail.com>
2023-10-02 01:19:19 +0300

    corrected error in calling search

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -58,38 +58,10 @@ class SessionDBAuth(SessionExpAuth):
         if not request:
             return None
         sess_id = self.session_cookie(request)
+
         if sess_id:
-            sess_objs_list = UserSession.search(session_id=sess_id)
-            if len(sess_obj_list) > 0:
-                sess_obj_list[0].remove()
+            sess_objs_list = UserSession.search({"session_id": sess_id})
+            if len(sess_objs_list) > 0:
+                sess_objs_list[0].remove()
                 return True
         return False
-
-    def save_to_file(self, session_id=None):
-        """ Saves the session_ID-user_ID dictionary to a file
-        """
-        filepath = ".db_session.json"
-        if not session_id:
-            return
-        session_data = self.user_id_by_session_id
-        sess_dct = session_data[session_id]
-        for key, val in sess_dct.items():
-            if type(val) is datetime:
-                session_data[session_id][key] = val.strftime(TIMESTAMP_FORMAT)
-
-        with open(filepath, 'w') as f:
-            json.dump(session_data, f)
-
-    def load_from_file(self):
-        """ Loads session data from file
-        """
-        filepath = ".db_session.json"
-        if path.exists(filepath):
-            with open(filepath, 'r') as f:
-                loaded_sess_data = json.load(f)
-            for key, val in loaded_sess_data.items():
-                for k, v in val.items():
-                    if k == 'created_at':
-                        loaded_sess_data[key][k] =\
-                                datetime.strptime(v, TIMESTAMP_FORMAT)
-            self.user_id_by_session_id = loaded_sess_data
